fb2a4c7608de9ba9bf88ea843dc2405e5be0fcd8
shariq-gazdar <gazdarshariq0@gmail.com>
2024-09-26 21:18:30 +0500

    ////

--- Javascript/e_commerce_sheets/app.js
+++ Javascript/e_commerce_sheets/app.js
@@ -1,3 +1,14 @@
 let cardContainer = document.querySelector(".cardContainer");
 let apiKey = "AIzaSyAIxEqW5gFeiCTKEYO9ITdAXaFdLlQbJG8";
-let api = "";
+let sheetId = `1OOv3VELEKLIj-5jerSJ7YmLezNOldqEjlDMdW7GZ1FY`;
+let api = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/A2:C5?key=${apiKey}`;
+fetch(api).then((res) => {
+  res.json().then((res) => {
+    console.log(res);
+    let data = res.values;
+    console.log(data);
+    for (let d = 0; d < data.length; d++) {
+      console.log(data[d]);
+    }
+  });
+});
