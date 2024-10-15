f51f0514798051f9f6731f7382391bb14ecf35f5
= <gazdarshariq0@gmail.com>
2024-10-15 13:00:45 +0500

    things add!!!!!!!!

---
+++ Javascript/switch/app.js
@@ -0,0 +1,26 @@
+// Create a cars object
+// Make them in the table
+
+const cars = {
+  "Ford Fusion": {
+    make: "Ford",
+    model: "Fusion",
+    year: 2019,
+    price: 30000,
+    mileage: 10000,
+    color: "Blue",
+    type: "Sedan",
+    features: ["Automatic", "Power Windows", "Navigation System"],
+  },
+  "Tesla Model S": {
+    make: "Tesla",
+    model: "Model S",
+    year: 2020,
+    price: 50000,
+    mileage: 5000,
+    color: "White",
+    type: "Sedan",
+    features: ["Automatic", "Power Windows", "Navigation System", "Bluetooth"],
+  },
+  // Add more cars here...
+};
